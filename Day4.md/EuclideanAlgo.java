51b43fc85f934ecad7110d8e29bae2000e0818ef
PRai <83975944+Priyanshi-Rai@users.noreply.github.com>
2023-02-04 11:10:12 +0530

    Update EuclideanAlgo.java

--- Day4.md/EuclideanAlgo.java
+++ Day4.md/EuclideanAlgo.java
@@ -1,1 +1,23 @@
+// SIMPLE VERSION OF EUCLIDEAN ALGORITHM
 
+int gcd(int a , int b){
+  while(a!=b){
+    if(a>b){
+      a=a-b;
+    }
+    else{
+      b=b-a;
+    }  
+  }
+  return a;
+}
+
+// OPTIMISED EUCLIDEAN ALGO
+int gcd(int a, int b){
+  if(b==0)
+    return a;
+  else
+    return gcd(b, a%b);
+}
+//first call is always swappping
+// Time complexity O(log(min(a,b)))
