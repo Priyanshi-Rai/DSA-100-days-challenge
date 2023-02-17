f7e2c5a3dbcf7673c2deb11ff5e6acee89bb8c6a
PRai <83975944+Priyanshi-Rai@users.noreply.github.com>
2023-02-17 11:43:39 +0530

    Optimised solution

--- Day4.md/prime.java
+++ Day4.md/prime.java
@@ -1,3 +1,4 @@
+}/** 
 public class prime {
     static boolean ifPrime(int n){
         if(n==1)
@@ -9,8 +10,31 @@ public class prime {
         return true;
     } 
     public static void main(String args[]){
-        int num = 4;
+        int num = 1234543217;
         System.out.println(ifPrime(num));
 
+    }
+} 
+*/
+
+//OPTIMISED SOLUTION FOR CHECKING PRIME NUMBER
+
+public class prime{
+    static boolean ifPrime(int num){
+        if(num==1)
+            return false;
+        if(num==2 || num==3)
+            return true;    
+        if(num%2==0 || num%3==0)
+            return false;
+        for(int i=5; i*i<num; i=i+6){
+            if(num%1==0 || num%(i+2)==0)
+                return false;
+        }    
+        return true;
+    }
+    public static void main(String args[]){
+        int num = 1234543217;
+        System.out.println(ifPrime(num));
     }
 }
