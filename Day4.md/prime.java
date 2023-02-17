7b6776091004f96ef6a4f325965eac7cd4a62a9f
PRai <83975944+Priyanshi-Rai@users.noreply.github.com>
2023-02-17 11:27:11 +0530

    Update prime.java

--- Day4.md/prime.java
+++ Day4.md/prime.java
@@ -2,7 +2,7 @@ public class prime {
     static boolean ifPrime(int n){
         if(n==1)
             return false;
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<n;i++){
             if(n%i==0)
                 return false;
         }
