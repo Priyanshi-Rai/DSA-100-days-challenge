d7a8b7bec86114b50483a4d5ba765165bbc2b7a5
PRai <83975944+Priyanshi-Rai@users.noreply.github.com>
2023-02-17 11:25:14 +0530

    Create prime.java

---
+++ Day4.md/prime.java
@@ -0,0 +1,16 @@
+public class prime {
+    static boolean ifPrime(int n){
+        if(n==1)
+            return false;
+        for(int i=2;i<n;i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    } 
+    public static void main(String args[]){
+        int num = 4;
+        System.out.println(ifPrime(num));
+
+    }
+}
